d725fe8657fb50311ee5ce18855b0c47126f5bf8
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-01-22 11:11:19 -0600

    add HTML CSS and JS files and assets folder.

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var API_key = "33f031fef3c3c8b1b91d38ca6a1b278c";
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=' + API_key;
+
+var city;
+var lat;
+var long;
