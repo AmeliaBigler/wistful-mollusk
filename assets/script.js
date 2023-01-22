e96738ee2f819212a57253742d56833609c45853
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-01-22 17:24:07 -0600

    add function to fetch API.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
 
 $(function() {
     var API_key = '33f031fef3c3c8b1b91d38ca6a1b278c';
-    var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + API_key;
 
-    var city;
     var lat;
     var lon;
 
@@ -21,17 +19,17 @@ $(function() {
     
         localStorage.setItem('city', JSON.stringify(city));
     
-        renderSearch();
+        renderSearchHistory();
+        getAPI();
     })
 
-    function renderSearch(){
+    function renderSearchHistory(){
         var searchedCity = JSON.parse(localStorage.getItem('city'));
         var cityArray = JSON.parse(localStorage.getItem('cityArray'));
 
         if (searchedCity !== null) {
             cityArray.push(searchedCity);
             localStorage.setItem('cityArray', JSON.stringify(cityArray));
-            localStorage.setItem('city', null);
         }
 
         if (cityArray !== null){
@@ -44,8 +42,42 @@ $(function() {
         } 
     }
 
+    function getAPI() {
+        var city = JSON.parse(localStorage.getItem('city')).city;
+
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + API_key;
+        fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            $('#current').empty();
+            console.log(data);
+            lon = data.coord.lon;
+            lat = data.coord.lat;
+            var cityName = $('<p></p>').text(data.name);
+            var iconCode = data.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            var icon = $('<img src="">').attr('src', iconURL);
+            var temp = $('<p></p>').text(data.main.temp + " degrees Fahrenheit");
+            var humid = $('<p></p>').text(data.main.humidity + "%");
+            var wind = $('<p></p>').text(data.wind.speed + " mph");
+            $('#current').append(cityName, icon, temp, humid, wind);
+
+        })
+
+        // var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + API_key;
+        // fetch(queryURL)
+        // .then(function (response) {
+        //     return response.json();
+        // })
+        // .then(function (data) {
+        //     console.log(data);
+        // })
+    }    
+
     function init(){
-        renderSearch();
+        renderSearchHistory();
     }
 
     init();
